9614286fa9e026fdf5c3a03aabb1a9c9755688d8
Elena Olsamo <elena.olsamo@cgi.com>
2018-04-14 10:28:27 +0300

    'Test2'

--- src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
@@ -22,7 +22,7 @@ import javax.persistence.MappedSuperclass;
 /**
  * Simple JavaBean domain object adds a name property to <code>BaseEntity</code>. Used as a base class for objects
  * needing these properties.
- *
+ * test
  * @author Ken Krebs
  * @author Juergen Hoeller
  */
